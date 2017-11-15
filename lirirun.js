77b6280d96140b3d61f5dabf5475eed4820989ea
unknown <smugclimber@icloud.com>
2017-11-14 22:04:01 -0500

    third commit with movie funct

--- lirirun.js
+++ lirirun.js
@@ -1,4 +1,4 @@
-console.log("\nWelcome to the LIRI!!!\n(Language Interpretation and Recognition Interface)\n==================================================\nCommand Options:\n==================================================\n1)'my-tweets'\nThis will show your last 20 tweets and when they were created\n\n2)'spotify-this-song' <input song name here>\nThis will show the following information about the song: Artist ,Song name, A Song Preview Link, and the Song Album.\n\n3)'movie-this' <input movie name here>\nThis will give the following info for a movie:\nTitle of the movie\nYear the movie came out\nIMDB Rating of the movie\nRotten Tomatoes Rating of the movie\nCountry where the movie was produced\nLanguage of the movie\nPlot of the movie\nActors in the movie.\n\n4)'do-what-it-says'\nThis will randomly call a LIRI command for you.\n==================================================\n" )
+console.log("\nWelcome to the LIRI!!!\n(Language Interpretation and Recognition Interface)\n==================================================\nCommand Options:\n==================================================\n1)'my-tweets'\nThis will show your last 20 tweets and when they were created\n\n2)'spotify-this-song' <input song name here>\nThis will show the following information about the song: Artist ,Song name, A Song Preview Link, and the Song Album.\n\n3)'movie-this' <input movie name here>\nThis will give you lots of info for a movie you select. \n\n4)'do-what-it-says'\nThis will randomly call a LIRI command for you.\n==================================================\n" )
 
 var mykeys = require("./keys.js");
 var fs = require("fs");
@@ -38,15 +38,31 @@ function spotSong(){
 };
 
 function movieThis(){
-  console.log("\nCommand funct movieThis was done.");
-  var movieName = "Jaws";
-  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-  console.log(queryUrl);
+  console.log("\nCommand funct movieThis was started.");
 
-  request(queryUrl, function (error, response, body) {
-    console.log('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    console.log('body:', body);
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "movieName",
+      message: "Type in a movie name: "
+    }
+  ]).then(function(inquirerResponse) {
+      var queryUrl = "http://www.omdbapi.com/?t=" + inquirerResponse.movieName + "&apikey=40e9cece";
+      console.log(queryUrl);
+      request(queryUrl, function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log("\n==================================================\n");
+        var movieInfo = JSON.parse(body);
+        console.log('Title:', movieInfo.Title);
+        console.log('Year:', movieInfo.Year);
+        console.log('Rated:', movieInfo.Rated);
+        console.log('Rotten Tomatoes:', movieInfo.Ratings[1].Value);
+        console.log('Production Country:', movieInfo.Country);
+        console.log('Language:', movieInfo.Language);
+        console.log('Plot:', movieInfo.Plot);
+        console.log('Leading Actors:', movieInfo.Actors);
+      });
   });
 };
 
